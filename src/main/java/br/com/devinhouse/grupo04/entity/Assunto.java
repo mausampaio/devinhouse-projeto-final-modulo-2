f5c2e7f0973e613946dd469891a9de231d6decc5
Mauricio Sampaio <mausampaio@gmail.com>
2021-05-18 11:30:22 -0300

    feat: adiciona entidade assunto

---
+++ src/main/java/br/com/devinhouse/grupo04/entity/Assunto.java
@@ -0,0 +1,55 @@
+package br.com.devinhouse.grupo04.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "assuntos")
+public class Assunto implements Serializable {
+
+	private static final long serialVersionUID = -1783220061547537590L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String descricao;
+	private LocalDate dtCadastro;
+	private char flAtivo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDtCadastro() {
+		return dtCadastro;
+	}
+
+	public void setDtCadastro(LocalDate dtCadastro) {
+		this.dtCadastro = dtCadastro;
+	}
+
+	public char getFlAtivo() {
+		return flAtivo;
+	}
+
+	public void setFlAtivo(char flAtivo) {
+		this.flAtivo = flAtivo;
+	}
+
+}
