8f1a1eede2c2671b722d2ae9a5ef0168817fb5a6
Mauricio Sampaio <mausampaio@gmail.com>
2021-05-25 22:51:20 -0300

    test: cria teste para ProcessoMapper

--- src/main/java/br/com/devinhouse/grupo04/dto/ProcessoDTOInput.java
+++ src/main/java/br/com/devinhouse/grupo04/dto/ProcessoDTOInput.java
@@ -30,6 +30,16 @@ public class ProcessoDTOInput implements Serializable {
 	public ProcessoDTOInput() {
 	}
 
+	public ProcessoDTOInput(@NotNull @Size(min = 1, max = 4) String sgOrgaoSetor,
+			@NotNull @Size(min = 1, max = 4) String nuAno, @NotNull @Size(min = 1, max = 250) String descricao,
+			@NotNull Long cdAssuntoId, @NotNull Long cdInteressadoId) {
+		this.sgOrgaoSetor = sgOrgaoSetor;
+		this.nuAno = nuAno;
+		this.descricao = descricao;
+		this.cdAssuntoId = cdAssuntoId;
+		this.cdInteressadoId = cdInteressadoId;
+	}
+
 	public String getSgOrgaoSetor() {
 		return sgOrgaoSetor;
 	}
